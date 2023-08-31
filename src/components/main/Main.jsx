6fd2928cf2c8cac9b04b595427b80e6fd09315b2
Yevhenii Vyshnevskyi <rochithefox@gmail.com>
2023-08-31 08:07:11 +0300

    Added: added text display when typing

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 import "./index.css";
 
 function Main() {
+  const [markdownText, setMarkdownText] = useState("");
+
+  const handleTextChange = (event) => {
+    setMarkdownText(event.target.value);
+  };
+
   return (
     <div className="main">
       <div className="main__left-side">
@@ -8,7 +15,10 @@ function Main() {
           <div className="markdown__title-block">
             <h2 className="markdown__title">Markdown</h2>
           </div>
-          <textarea className="markdown__textarea"></textarea>
+          <textarea
+            className="markdown__textarea"
+            onChange={handleTextChange}
+          ></textarea>
         </div>
       </div>
       <div className="main__right-side">
@@ -17,7 +27,7 @@ function Main() {
             <h2 className="preview__title">Preview</h2>
             <button className="preview__show-btn preview__show-btn_active" />
           </div>
-          <div className="preview__text"></div>
+          <div className="preview__text">{markdownText}</div>
         </div>
       </div>
     </div>
