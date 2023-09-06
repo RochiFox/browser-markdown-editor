dd213360d3e42e80287a269842ae8566f9ddf65c
Yevhenii Vyshnevskyi <rochithefox@gmail.com>
2023-09-06 09:45:10 +0300

    Added: added save popup

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,6 +11,14 @@ function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
   const [fileName, setFileName] = useState(data[0].name);
+  const [isSaveMessageVisible, setSaveMessageVisible] = useState(false);
+
+  const showSaveMessage = () => {
+    setSaveMessageVisible(true);
+    setTimeout(() => {
+      setSaveMessageVisible(false);
+    }, 3000);
+  };
 
   const openPopup = () => {
     setIsPopupOpen(true);
@@ -58,11 +66,14 @@ function Header() {
       <div className="header__right-side">
         <button className="header__delete-btn" onClick={openPopup} />
         <DeletePopupMenu isOpen={isPopupOpen} onClose={closePopup} />
-        <button className="header__save-btn">
+        <button className="header__save-btn" onClick={showSaveMessage}>
           <img src={SaveLogo} alt="save logo" className="header__save-logo" />
           Save Changes
         </button>
       </div>
+      {isSaveMessageVisible && (
+        <div className="save-message">Changed saves</div>
+      )}
     </div>
   );
 }
