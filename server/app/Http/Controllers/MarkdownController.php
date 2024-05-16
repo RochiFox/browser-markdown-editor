9c5cb4bda41c400c7897f41b27e7085ca57d1270
RochiFox <rochithefox@gmail.com>
2024-05-16 14:41:13 +0300

    Added: added migration, api and controller for markdown

---
+++ server/app/Http/Controllers/MarkdownController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Markdown;
+
+class MarkdownController extends Controller
+{
+    //
+}
---
+++ server/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MarkdownController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('markdown', [MarkdownController::class, 'index']);
+Route::post('markdown/add', [MarkdownController::class, 'store']);
+Route::put('markdown/update/{id}', [MarkdownController::class, 'update']);
+Route::delete('markdown/delete/{id}', [MarkdownController::class, 'destroy']);
