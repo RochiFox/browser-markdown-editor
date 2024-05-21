e9c6b4f107474f6063b875ff72e8b22f8b43f9b0
RochiFox <rochithefox@gmail.com>
2024-05-21 13:41:34 +0300

    Added: added seeder to create a text for table

--- server/database/migrations/2024_05_16_100754_create_markdown_table.php
+++ server/database/migrations/2024_05_16_100754_create_markdown_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Artisan;
 
 return new class extends Migration
 {
@@ -17,6 +18,10 @@ return new class extends Migration
             $table->longText('text');
             $table->timestamps();
         });
+
+        Artisan::call('db:seed', [
+            '--class' => 'MarkdownTableSeeder',
+        ]);
     }
 
     /**
